f440b2b683a2a63728ff4e81e5c895bb8c97d2ed
mnxmnz <minzidev@gmail.com>
2025-06-06 00:09:12 +0900

    feat: 국가 바 표시 애니메이션 추가 및 DOMContentLoaded 이벤트로 초기화

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-function showTopBar(){
-    let country = "France";
-    let vat = 20;
-    setTimeout(() => {
-        document.querySelector("section.country-bar")
-        .innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`
-        document.querySelector("section.country-bar")
-        .classList.remove('hidden')
-    }, 1000);
+function showTopBar() {
+  let country = 'France';
+  let vat = 20;
+
+  const countryBar = document.querySelector('section.country-bar');
+
+  if (countryBar) {
+    countryBar.innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
+    countryBar.classList.remove('hidden');
+    countryBar.offsetHeight;
+    countryBar.classList.add('visible');
+  }
 }
 
-
-
-showTopBar();
-
-
-
-
+document.addEventListener('DOMContentLoaded', showTopBar);
